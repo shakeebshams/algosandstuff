965bc9a0e1aeab23edad8ca1ecb12447bbc2fa61
shakeebshams <shakeebshams@gmail.com>
2020-05-05 22:50:59 -0400

    complete reversestring

--- qs/reversestring/index.js
+++ qs/reversestring/index.js
@@ -6,6 +6,38 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse1(str) {
+    var arr = str.split('');
+    var reverseArr = arr.reverse();
+    return reverseArr.join('');
+}
+
+function reverse2(str) {
+    let reversed = '';
+
+    for (let character of str) {
+        reversed = character + reversed;
+    }
+
+    return reversed;
+
+}
+
+function reverse3(str) {
+    var reversed = '';
+    return str.split('').reduce((reversed, character) => {
+        return character + reversed;
+    }, '');
+}
+
+function reverse(str) {
+    let arr = str.split('');
+    let reversedString = '';
+    for (var i = arr.length - 1; i > -1; i--) {
+        reversedString += arr[i];
+    }
+    console.log(reversedString);
+    return reversedString;
+}
 
 module.exports = reverse;
